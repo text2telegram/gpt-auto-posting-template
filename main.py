d3f5d172247d343bca4b8a0d030fe71ecac1a011
text2telegram <text2telegram@gmail.com>
2025-06-03 23:12:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+from send_to_telegram import send_post
 from generate_text import generate_hook, generate_lesson
 from generate_voice import generate_voice
-from send_to_telegram import send_post
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 if __name__ == "__main__":
     print("Bot is running...")
@@ -9,3 +15,4 @@ if __name__ == "__main__":
     lesson = generate_lesson()
     audio_path = generate_voice(lesson)
     send_post(hook, lesson, audio_path)
+

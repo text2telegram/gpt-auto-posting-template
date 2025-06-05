bcc5f4e0f12e6da08a4d50df67b55aa861f7c193
text2telegram <text2telegram@gmail.com>
2025-06-05 12:00:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 
-import os
-import requests
-import time
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TARGET_CHAT_ID")
-TEXT = os.getenv("POST_TEXT", "🧠 SmartTestBot готов!")
-
-def send_message():
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": TEXT, "parse_mode": "Markdown"}
-    response = requests.post(url, json=payload)
-    print(response.text)
+from generate_text import generate_text
+from generate_voice import generate_voice
+from send_to_telegram import send_post
 
 if __name__ == "__main__":
-    while True:
-        send_message()
-        time.sleep(86400)  # каждые 24 часа
+    generate_text()
+    generate_voice()
+    send_post()
+
 
 
